f50cb9612f9e28c09ca8770c67ece11c0cd8f742
susanliubc <susanliubc@gmail.com>
2018-12-04 16:10:54 -0800

    add Home page and Navbar

---
+++ books/src/components/pages/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Home = (props) => {
+    return (
+        <div className="container">
+            <h4 className="center">Favorite Books Home</h4>
+            <p>This website provide a place to show my favorite books from www.goodreads.com. It is never too late to learn knowledges. Feel free to share it with other people</p>
+        </div>
+    )
+};
+
+export default Home;
+
---
+++ books/src/components/pages/Navbar.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Link, NavLink, withRouter } from 'react-router-dom';
+
+const Navbar = (props) = (
+        <nav className="nav-wrapper purpule ligthen-3">
+            <div className="container">
+                <Link className='brand-logo' to='/'>Favorite Books</Link>
+                <ul className="right">
+                    <li><NavLink exact to='/'>Home</NavLink></li>
+                    <li><NavLink to='/dashboard'>Dashboard</NavLink></li>
+                    <li><NavLink to='/newbook'>New Book</NavLink></li>
+                </ul>
+            </div>
+        </nav>
+);
+
+export default withRouter(Navbar);
